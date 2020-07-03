51310dd390b6e1dd3fe264ee9b83572e1cde2991
ovaphlow <ovaphlow@outlook.com>
2020-07-03 17:15:49 +0800

    bulma with flex in progress

---
+++ webapp/ui/src/Component.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome, faArchive, faBox, faHandPointUp, faHistory, faUserCircle, faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
+
+// eslint-disable-next-line import/prefer-default-export
+export function TopNav() {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="container is-fluid">
+        <div className="navbar-brand">
+          <a href="home.html" className="navbar-item">
+            OVAPHLOW
+          </a>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a href="home.html" className="navbar-item">
+              <FontAwesomeIcon icon={faHome} fixedWidth />
+              首页
+            </a>
+
+            <a href="archive.html" className="navbar-item">
+              <FontAwesomeIcon icon={faArchive} fixedWidth />
+              档案管理
+            </a>
+
+            <a href="archive-isolated.html" className="navbar-item">
+              <FontAwesomeIcon icon={faBox} fixedWidth />
+              档案中转库
+            </a>
+
+            <a href="touch-screen.html" className="navbar-item">
+              <FontAwesomeIcon icon={faHandPointUp} fixedWidth />
+              触摸屏查询终端
+            </a>
+
+            <a href="api-journal.html" className="navbar-item">
+              <FontAwesomeIcon icon={faHistory} fixedWidth />
+              API 调用记录
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a href="current-user.html" className="navbar-link">
+                <FontAwesomeIcon icon={faUserCircle} fixedWidth />
+                当前用户
+              </a>
+
+              <div className="navbar-dropdown">
+                <a href="current-user.html" className="navbar-item">
+                  <FontAwesomeIcon icon={faUserCircle} fixedWidth />
+                  用户信息
+                </a>
+
+                <hr className="navbar-divider" />
+
+                <a href="sign-in.html" className="navbar-item">
+                  <FontAwesomeIcon icon={faSignOutAlt} fixedWidth />
+                  退出登录
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export function LeftNav() {
+  return (
+    <div className="box">
+      <aside className="menu" style={{ width: '240px' }}>
+        <p className="menu-label">档案功能</p>
+        <ul className="menu-list">
+          <li>
+            <a href="archive.html">
+              档案
+            </a>
+          </li>
+
+          <li>
+            <a href="archive.html">
+              档案中转
+            </a>
+          </li>
+        </ul>
+        <p className="menu-label">设备</p>
+        <ul className="menu-list">
+          <li>
+            <a href="archive.html">
+              触摸屏
+            </a>
+          </li>
+        </ul>
+        <p className="menu-label">其它</p>
+        <ul className="menu-list">
+          <li>
+            <a href="archive.html">
+              API
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  );
+}
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>Bulma</strong>
+          {' '}
+          by
+          <a href="https://jgthms.com">Jeremy Thomas</a>
+          . The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
+          . The website content
+          is licensed
+          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+}
--- webapp/ui/src/test/index.jsx
+++ webapp/ui/src/test/index.jsx
@@ -1,134 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { TopNav, LeftNav } from '../Component';
 
 ReactDOM.render(<Index />, document.getElementById('app'));
 
 function Index() {
   return (
-    <div className="d-flex flex-column h-100 w-100">
-      <header>
-        <nav className="navbar navbar-expand navbar-dark fixed-top sticky-top bg-dark mb-3">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#/">Fixed navbar</a>
-          </div>
-        </nav>
-      </header>
+    <React.Fragment key="app">
+      <div className="tile is-ancestor">
+        <div className="tile is-parent">
+          <article className="tile is-child">
+            <TopNav />
+          </article>
+        </div>
+      </div>
 
-      <main className="flex-grow-1">
-        <div className="container-fluid h-100">
-          <div className="row h-100 d-flex justify-content-center">
-            <div className="col-3 col-lg-2">
-              <div className="card bg-dark h-100">
-                <div className="card-body">
-                  <ul className="nav flex-column">
-                    <li className="nav-item">
-                      <a className="nav-link text-reset text-decoration-none" aria-current="page" href="#2">
-                        Dashboard
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link active text-reset text-decoration-none" aria-current="page" href="#2">
-                        <strong>&gt; Dashboard &lt;</strong>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+      <div className="tile is-ancestor hh">
+        <div className="tile is-vertical is-2">
+          <div className="tile">
+            <div className="tile is-parent is-vertical">
+              <article className="tile is-child box" style={{ flexShrink: '1' }}>
+                USER & NOTIFICATION
+              </article>
 
-            <div className="col">
-              <div className="container-lg h-100 d-flex flex-column">
-                <div className="d-flex justify-content-between alitn-items-end">
-                  <div className="btn-group">
-                    <button type="button" className="btn btn-link text-reset text-decoration-none">
-                      <i className="fa fa-fw fa-angle-left" />
-                      后退
-                    </button>
-                  </div>
-                  <span className="h1">12213</span>
-                  <ul className="list-inline align-self-end">
-                    <li className="list-inline-item">1123</li>
-                    <li className="list-inline-item">2234</li>
-                  </ul>
-                </div>
-                <div className="card bg-dark h-100">
-                  <div className="card-header">
-                    HEADER
-                  </div>
-                  <div className="card-body">
-                    <div className="mb-3">
-                      <label className="form-label">NAME</label>
-                      <input type="text" placeholder="1123123123" className="form-control rounded-0 input-underscore" />
-                    </div>
-                    1
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                    <p className="lead">89</p>
-                  </div>
-                  <div className="card-footer">
-                    FOOTER
-                  </div>
-                </div>
-              </div>
-              {/* <div className="card shadow bg-dark h-100 w-100 workspace">
-                <div className="card-body">
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                  <p className="lead">89</p>
-                </div>
-              </div> */}
+              <article className="tile is-child hh">
+                <LeftNav />
+              </article>
             </div>
           </div>
         </div>
-      </main>
 
-      <footer className="mt-3 bg-dark">
-        <div className="container-fluid">
-          <p className="lead py-0">
-            Copyright
-          </p>
+        <div className="tile is-parent hh">
+          <article className="tile is-child box hh">
+            WORKSPACE
+          </article>
         </div>
-      </footer>
-    </div>
+      </div>
+
+      <div className="tile is-ancestor">
+        <div className="tile is-parent">
+          <article className="tile box is-child">
+            FOOTER
+          </article>
+        </div>
+      </div>
+
+      {/* <section>
+        <TopNav />
+      </section> */}
+
+      {/* <section>
+        <div className="container is-fluid">
+          <div className="columns mt-4">
+            <div className="column is-narrow">
+              <LeftNav />
+            </div>
+
+            <div className="column">
+              <div className="container is-desktop">
+                <div className="box">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> */}
+    </React.Fragment>
   );
 }
